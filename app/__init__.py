c2d4a85c55b979bc927d968984449f6396588df3
Joy Hwang <joyhwang10@gmail.com>
2021-07-18 15:00:28 -0400

    create new user + save high score

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 import os
 import ast
 
+
 # CONFIG
 app = Flask(__name__)
 CORS(app)
@@ -21,17 +22,22 @@ class User(db.Document):
     username = db.StringField()
     high_score = db.StringField()
     def to_json(self):
-        return {"username": self.name,
+        return {"username": self.username,
                 "high_score": self.high_score}
 
 
 # ROUTES
 @app.route('/user', methods=['GET'])
 def get_user():
-    username = request.args.get('username')
-    user = User.objects(username=username)
+    req_username = request.args.get('username')
+    user = User.objects(username=req_username)
     if not user:
-        return jsonify({'Do': 'Something'}) # todo add user to db
+        new_user = User(
+            username=req_username,
+            high_score="0"
+        )
+        new_user.save()
+        return jsonify(new_user.to_json())
     else:
         resp = {
             'username': user[0]['username'],
@@ -45,14 +51,15 @@ def check_score():
     body_dict = ast.literal_eval(req_body)
     resp = ''
     user = User.objects(username=body_dict['user'])
+
     if not user:
-        return jsonify({'Do': 'Something'}) # todo add user to db
+        return jsonify({'Do': 'Something'}) # todo error handling
     else:
         candidate = body_dict['newScore']
         current = user[0]['high_score']
         if candidate > current:
             resp = {"message": f"New high score! {candidate} beats {current}"}
-            # todo save new high score 
+            user.update(high_score=candidate)
         else:
             resp = {"message": f"{candidate} doesn't beat your current high score of {current}. Try again!"}
     return jsonify(resp)
