83eea78315ced9face10089784435f3bc81cd81c
dli86 <dli86@users.noreply.github.com>
2021-07-31 17:53:58 -0400

    Add start game, player turn rotation, chat capability

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ db.init_app(app)
 
 # Local server state
 class GameState():
-    usernames = set()
+    usernames = []
     high_score_map = [] # Map from username to their high score. We use a map instead of an object list since it is easier to to perform lookups. 
     current_score_map = []
     user_with_turn = ""
@@ -25,6 +25,10 @@ class GameState():
     chat_messages = []
     has_game_started = False
     error_message = ""
+    game_status_message = "Game has not started."
+
+    # Server-side only game state
+    turn_idx = 0
 
     def to_json(self):
         return json.dumps(
@@ -36,7 +40,8 @@ class GameState():
                 "game_transcript": self.game_transcript,
                 "chat_messages": self.chat_messages,
                 "has_game_started": self.has_game_started,
-                "error_message": self.error_message
+                "error_message": self.error_message,
+                "game_status_message": self.game_status_message
             }
         )
 
@@ -58,9 +63,16 @@ def get_user():
     user = User.objects(username=req_username)
 
     if req_username not in game_state.usernames:
-        game_state.usernames.add(req_username)
+        game_state.usernames.append(req_username)
+        game_state.chat_messages.append(f"{req_username} joined. ")
     else:
-        error_msg = f"{req_username} is already logged in. "
+        error_msg = f"{req_username} is already logged in."
+        print(error_msg)
+        game_state.error_message = error_msg
+        return get_game_state()
+
+    if game_state.has_game_started:
+        error_msg = "Game has already started."
         print(error_msg)
         game_state.error_message = error_msg
         return get_game_state()
@@ -110,9 +122,40 @@ from flask_socketio import SocketIO, send, emit
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('join')
-def handle_joined(user):
-    print(f"{user} joined the game")
-    game_state.chat_messages.append(f"{user} joined the game. ")
+def handle_joined():
+    print("handle_joined")
+    emit('broadcast_game_state', get_game_state(), broadcast=True)
+
+@socketio.on('get_user_with_current_turn')
+def get_user_with_current_turn():
+    print("get_user_with_current_turn")
+    game_state.user_with_turn = game_state.usernames[game_state.turn_idx]
+    game_state.game_status_message = "Game is in progress. " + game_state.user_with_turn + " has the current turn."
+    emit('broadcast_game_state', get_game_state(), broadcast=True)
+
+@socketio.on('start_game')
+def handle_start_game():
+    print("start_game")
+    game_state.has_game_started = True
+    game_state.user_with_turn = game_state.usernames[game_state.turn_idx]
+    game_state.game_status_message = "Game is in progress. " + game_state.user_with_turn + " has the current turn."
+    emit('broadcast_game_state', get_game_state(), broadcast=True)
+
+@socketio.on('chat_message')
+def handle_chat_message(user, user_message):
+    print(f"{user} sent message {user_message}.")
+    game_state.chat_messages.append(f"{user}: {user_message}.")
+    emit('broadcast_game_state', get_game_state(), broadcast=True)
+
+@socketio.on('end_turn')
+def handle_end_turn(user, player_score):
+    print("handle_end_turn")
+    print(f"{user} score {player_score}.")
+    # Place the user score in to the score map
+    # Update the transcript with the user score info
+    game_state.turn_idx = (game_state.turn_idx + 1) % len(game_state.usernames)
+    game_state.user_with_turn = game_state.usernames[game_state.turn_idx]
+    game_state.game_status_message = "Game is in progress. " + game_state.user_with_turn + " has the current turn."
     emit('broadcast_game_state', get_game_state(), broadcast=True)
 
 def get_game_state():
