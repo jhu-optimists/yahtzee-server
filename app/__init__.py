0fc177a71017f8f0e73b2f0df60ae764a1c6d95f
dli86 <dli86@users.noreply.github.com>
2021-08-07 13:00:39 -0400

    Add current player's status

--- app/__init__.py
+++ app/__init__.py
@@ -69,6 +69,7 @@ def get_user():
     if req_username not in game_state.usernames:
         game_state.usernames.append(req_username)
         game_state.transcript.append(f"{req_username} joined. ")
+        game_state.current_score_map[req_username] = { "total": 0 }
     else:
         error_msg = f"{req_username} is already logged in."
         print(error_msg)
@@ -134,7 +135,8 @@ def handle_joined():
 def get_user_with_current_turn():
     print("get_user_with_current_turn")
     game_state.user_with_turn = game_state.usernames[game_state.turn_idx]
-    game_state.game_status_message = "Game is in progress. " + game_state.user_with_turn + " has the current turn."
+    user = game_state.user_with_turn
+    game_state.game_status_message = f"{user} has the current turn. Score: {game_state.current_score_map[user]}. Dice values: {game_state.dice_values}."
     emit('broadcast_game_state', get_game_state(), broadcast=True)
 
 @socketio.on('start_game')
@@ -142,8 +144,9 @@ def handle_start_game():
     print("start_game")
     game_state.has_game_started = True
     game_state.user_with_turn = game_state.usernames[game_state.turn_idx]
-    game_state.game_status_message = "Game is in progress. " + game_state.user_with_turn + " has the current turn."
-    game_state.transcript.append("Game is in progress. " + game_state.user_with_turn + " has the current turn.")
+    user = game_state.user_with_turn
+    game_state.game_status_message = f"{user} has the current turn. Score: {game_state.current_score_map[user]}. Dice values: {game_state.dice_values}."
+    game_state.transcript.append(game_state.user_with_turn + " has the current turn.")
     emit('broadcast_game_state', get_game_state(), broadcast=True)
 
 @socketio.on('chat_message')
@@ -158,12 +161,14 @@ def handle_end_turn(user, player_score):
     print(f"{user} score {player_score}.")
     # Place the user score in to the score map
     # Update the transcript with the user score info
-    game_state.turn_idx = (game_state.turn_idx + 1) % len(game_state.usernames)
-    game_state.user_with_turn = game_state.usernames[game_state.turn_idx]
-    game_state.game_status_message = "Game is in progress. " + game_state.user_with_turn + " has the current turn."
-    game_state.transcript.append(f"Turn has ended for {user}. {user}'s score: {player_score}. Now " + game_state.user_with_turn + " has the current turn.")
     game_state.current_score_map[user] = player_score
     game_state.dice_roll_count = 0
+    game_state.dice_values = []
+    game_state.turn_idx = (game_state.turn_idx + 1) % len(game_state.usernames)
+    next_user = game_state.usernames[game_state.turn_idx]
+    game_state.user_with_turn = next_user
+    game_state.game_status_message = f"{next_user} has the current turn. Score: {game_state.current_score_map[next_user]}. Dice values: {game_state.dice_values}."
+    game_state.transcript.append(f"Turn has ended for {user}. {user}'s score: {player_score}. Now " + next_user + " has the current turn.")
     emit('broadcast_game_state', get_game_state(), broadcast=True)
 
 @socketio.on('dice_values')
@@ -171,7 +176,9 @@ def handle_dice_values(dice_values):
     print(f"Dice values {dice_values}.")
     game_state.dice_values = dice_values
     game_state.dice_roll_count += 1
-    game_state.transcript.append(f"Dice values for {game_state.user_with_turn}: {dice_values}.")
+    user = game_state.user_with_turn
+    game_state.game_status_message = f"{user} has the current turn. Score: {game_state.current_score_map[user]}. Dice values: {dice_values}."
+    game_state.transcript.append(f"Dice values for {user}: {dice_values}.")
     emit('broadcast_game_state', get_game_state(), broadcast=True)
 
 def get_game_state():
